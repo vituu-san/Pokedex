b8f86fc4f0278ce9698339dfcf4f2500c330aef5
Vitor Costa <developer.deivide.costa@gmail.com>
2024-09-27 19:58:11 -0300

    created project

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Vitor Costa on 27/09/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
